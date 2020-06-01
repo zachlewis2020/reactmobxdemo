02c21c9a84153d37d88645a7004a95c418ce24b8
Zach Lewis <zachlewis@Zachs-MacBook-Pro.local>
2020-06-01 11:43:09 -0500

    added offline check

--- web/src/pages/Page1.tsx
+++ web/src/pages/Page1.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { RouteComponentProps } from "react-router-dom";
-import { Button } from "@material-ui/core";
-import { observable, action, computed } from "mobx"
+import {RouteComponentProps} from "react-router-dom";
+import {Button} from "@material-ui/core";
+import {observable, observe, action, computed} from "mobx"
+import NetInfo from "@react-native-community/netinfo";
 
-class DataStore {
-    @observable data: any
+interface DataStore {
+    name: string
 }
 
 class Page1 extends React.Component<RouteComponentProps, DataStore> {
-
-
-    @computed get data() {
-        alert('works:(' + navigator.onLine + ')')
-        return navigator.onLine
-    }
+    state = {
+     name: ''
+    };
 
     person = observable(
         {
             // observable properties:
             name: "John",
             age: 42,
+            online: navigator.onLine,
             // computed property:
             get labelText() {
                 return navigator.onLine ? `${this.name} (age: ${this.age})` : this.name
+            },
+
+            setName(name: string) {
+                this.name = name;
             },
 
             setAge(age: number) {
@@ -30,9 +33,40 @@ class Page1 extends React.Component<RouteComponentProps, DataStore> {
             }
         },
         {
+            setName: action,
             setAge: action
         }
     )
+
+    @computed get data() {
+        if ( navigator.onLine) {
+            return this.person.name  + " :Online"
+        } else {
+            return "Offline: " + this.person.name
+        }
+    }
+
+
+
+    unsubscribe = NetInfo.addEventListener(state => {
+        console.log("Connection type", state.type);
+        console.log("Is connected?", state.isConnected);
+        if (state.isConnected) {
+            this.person.name = "online now";
+            this.setState({name: `${this.person.name}`})
+            console.log("nbame: " + this.person.name)
+        } else {
+            this.person.setName("Offline now");
+            this.setState({name: `${this.person.name}`})
+            console.log("nbame: " + this.person.name)
+        }
+    });
+
+    isChanged = observe(this.person, change => {
+        console.log("Changed:" + `${change.type} ` )
+        return this.person.name
+    });
+
 
     render() {
 
@@ -40,26 +74,26 @@ class Page1 extends React.Component<RouteComponentProps, DataStore> {
             <div className="Page1">
                 <h1>Wecome to Home Page #1</h1>
                 <h2>{this.person.labelText}</h2>
-                <h1>Online:({this.data.toString()})</h1>
+                <h1>Online:({this.state.name})</h1>
                 <Button
                     variant="contained"
-                    style={{ color: 'green' }}
+                    style={{color: 'green'}}
                     onClick={() => {
                         this.props.history.push('/');
                     }}
                 >
                     Go To Home Page
-                    </Button>
+                </Button>
                 <p>Add Home Page Stuff Here</p>
                 <Button
                     variant="contained"
-                    style={{ color: 'green' }}
+                    style={{color: 'green'}}
                     onClick={() => {
                         this.props.history.push('/page2');
                     }}
                 >
                     Go To Page #2
-                    </Button>
+                </Button>
                 <p>Add Home Page Stuff Here</p>
             </div>
         );
